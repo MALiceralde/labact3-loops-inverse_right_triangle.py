ad5aac186554049ae2d9bdcd49169738af434297
MALiceralde <146396314+MALiceralde@users.noreply.github.com>
2023-10-13 19:18:07 +0800

    Add files via upload

---
+++ inverse_right_triangle.py
@@ -0,0 +1,10 @@
+# Number of rows in the pattern
+num_rows = 5
+
+for loop1 in range(num_rows, 0, -1):
+    for loop2 in range(1, loop1 + 1):
+        print(loop2, end=' ')  # will print the current number with a space
+    print()
+
+
+
